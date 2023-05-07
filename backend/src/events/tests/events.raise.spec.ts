37d257d9470dda3e1c7c3514138ab31c32845bf0
carlosedof <carlosedof@gmail.com>
2023-05-07 19:23:14 -0300

    tests: add second client connection for advanced mocking - testing multiple raise value requests and scenarios

--- backend/src/events/tests/events.raise.spec.ts
+++ backend/src/events/tests/events.raise.spec.ts
@@ -51,7 +51,7 @@ describe('Raise round value logic', () => {
 
   beforeEach(async () => {
     jest.clearAllMocks();
-    await connectionMock.start('Sayid Jarrah');
+    await connectionMock.start('Sayid Jarrah', 'John Locke');
   });
 
   afterEach(async () => {
@@ -61,7 +61,7 @@ describe('Raise round value logic', () => {
   it('should request raise, accept and add 3 points to score', (done) => {
     connectionMock.client.once('raiserequest', () => {
       connectionMock.client.emit('raiseresponse', {
-        response: true,
+        response: 'accept',
       });
     });
     connectionMock.client.once('raiseresponse', () => {
@@ -74,6 +74,149 @@ describe('Raise round value logic', () => {
       'scoreboard',
       ({ scoreboard }: { scoreboard: IScoreboard }) => {
         expect(scoreboard?.team2?.score).toBe(4);
+        done();
+      },
+    );
+    connectionMock.client.emit('raiserequest');
+  });
+
+  it('should request raise, deny and add 1 point to score', (done) => {
+    connectionMock.client.once('raiserequest', () => {
+      connectionMock.client.emit('raiseresponse', {
+        response: 'deny',
+      });
+    });
+    connectionMock.client.once(
+      'scoreboard',
+      ({ scoreboard }: { scoreboard: IScoreboard }) => {
+        expect(
+          scoreboard?.team1?.score === 1 && scoreboard?.team2?.score === 1,
+        ).toBeTruthy();
+        done();
+      },
+    );
+    connectionMock.client.emit('raiserequest');
+  });
+
+  it('should request raise, accept and the response is doubled', (done) => {
+    connectionMock.client.on('raiserequest', ({ team, user }) => {
+      if (user === 'Sayid Jarrah') {
+        connectionMock.client2.emit('raiseresponse', {
+          response: 'raise',
+        });
+      } else {
+        connectionMock.client.emit('raiseresponse', {
+          response: 'accept',
+        });
+      }
+    });
+    connectionMock.client.on(
+      'raiseresponse',
+      ({ response }: { response: string }) => {
+        if (response === 'accept') {
+          connectionMock.client.emit('played', {
+            card: { suit: 'h', nr: 'K' },
+            player: 'Sayid Jarrah',
+          });
+        }
+      },
+    );
+    connectionMock.client.once(
+      'scoreboard',
+      ({ scoreboard }: { scoreboard: IScoreboard }) => {
+        expect(scoreboard?.team2?.score).toBe(7);
+        done();
+      },
+    );
+    connectionMock.client.emit('raiserequest');
+  });
+
+  it('should request raise twice, accept and the response is raised twice', (done) => {
+    let raiseCount = 0;
+    connectionMock.client.on('raiserequest', ({ user }) => {
+      if (user === 'Sayid Jarrah') {
+        if (raiseCount === 0) {
+          connectionMock.client2.emit('raiseresponse', {
+            response: 'raise',
+          });
+        } else {
+          connectionMock.client2.emit('raiseresponse', {
+            response: 'accept',
+          });
+        }
+        raiseCount++;
+      } else {
+        connectionMock.client.emit('raiseresponse', {
+          response: 'raise',
+        });
+      }
+    });
+    connectionMock.client.on(
+      'raiseresponse',
+      ({ response }: { response: string }) => {
+        if (response === 'accept') {
+          connectionMock.client.emit('played', {
+            card: { suit: 'h', nr: 'K' },
+            player: 'Sayid Jarrah',
+          });
+        }
+      },
+    );
+    connectionMock.client.once(
+      'scoreboard',
+      ({ scoreboard }: { scoreboard: IScoreboard }) => {
+        expect(scoreboard?.team2?.score).toBe(10);
+        done();
+      },
+    );
+    connectionMock.client.emit('raiserequest');
+  });
+
+  it('should request raise three times, end round and reinitialize scoreboard', (done) => {
+    let raiseCount = 0;
+    let gameEnded = false;
+    connectionMock.client.on('raiserequest', ({ user }) => {
+      if (user === 'Sayid Jarrah') {
+        if (raiseCount === 2) {
+          connectionMock.client2.emit('raiseresponse', {
+            response: 'accept',
+          });
+        } else {
+          connectionMock.client2.emit('raiseresponse', {
+            response: 'raise',
+          });
+        }
+        raiseCount++;
+      } else {
+        connectionMock.client.emit('raiseresponse', {
+          response: 'raise',
+        });
+      }
+    });
+    connectionMock.client.on(
+      'raiseresponse',
+      ({ response }: { response: string }) => {
+        if (response === 'accept') {
+          connectionMock.client.emit('played', {
+            card: { suit: 'h', nr: 'K' },
+            player: 'Sayid Jarrah',
+          });
+        }
+      },
+    );
+    connectionMock.client.once(
+      'endgame',
+      ({ winnerTeam }: { winnerTeam: string }) => {
+        expect(winnerTeam).toBe('team2');
+        gameEnded = true;
+      },
+    );
+    connectionMock.client.once(
+      'scoreboard',
+      ({ scoreboard }: { scoreboard: IScoreboard }) => {
+        expect(scoreboard?.team2?.score).toBe(0);
+        expect(scoreboard?.team1?.score).toBe(0);
+        expect(gameEnded).toBeTruthy();
         done();
       },
     );
--- backend/src/events/tests/utils.ts
+++ backend/src/events/tests/utils.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsModule } from '../events.module';
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
+import { INestApplication } from '@nestjs/common';
 
 class ConnectionMock {
-  app;
+  app: INestApplication;
   server;
-  port;
-  client;
-  username;
+  port: number;
+  client: Socket<DefaultEventsMap, DefaultEventsMap>;
+  client2: Socket<DefaultEventsMap, DefaultEventsMap>;
+  username: string;
+  username2: string;
 
-  async start(_username) {
+  async start(_username: string, _username2?: string) {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [EventsModule],
     }).compile();
@@ -17,15 +21,22 @@ class ConnectionMock {
     this.app = await moduleFixture.createNestApplication();
     await this.app.listen(this.port);
     this.server = await this.app.getHttpServer();
-    await this.startClient(_username);
+    await this.startClient(_username, _username2);
   }
 
-  async startClient(username) {
+  async startClient(username: string, username2?: string) {
     this.username = username;
     this.client = io(`http://localhost:${this.port}`, {
       // @ts-ignore
-      query: `user=${username}}`,
+      query: `user=${username}`,
     });
+    if (username2) {
+      this.username2 = username2;
+      this.client2 = io(`http://localhost:${this.port}`, {
+        // @ts-ignore
+        query: `user=${username2}`,
+      });
+    }
   }
 
   async disconnect() {
